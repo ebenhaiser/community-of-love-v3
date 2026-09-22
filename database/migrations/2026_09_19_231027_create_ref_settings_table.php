6d5433a9cdaae3a62305b6d03e16818fe5ac0b56
Ebenhaiser J. Caprisiano <ebenhaiser@gmail.com>
2026-09-22 11:26:02 +0700

    update ref setting migration

--- database/migrations/2026_09_19_231027_create_ref_settings_table.php
+++ database/migrations/2026_09_19_231027_create_ref_settings_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('ref_settings', function (Blueprint $table) {
+            $table->id();
             $table->string('name')->primary();
             $table->string('type');
             $table->string('value');
