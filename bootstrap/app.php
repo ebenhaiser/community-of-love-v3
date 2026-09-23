7ae06dc1587f2647c44e4e5d841bf42370dd925b
Ebenhaiser J. Caprisiano <ebenhaiser@gmail.com>
2026-09-23 08:45:03 +0700

    edit app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,9 +25,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage')
-    ->useBootstrapFilesPath($_ENV['APP_BOOTSTRAP'] ?? '/tmp')
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -41,3 +39,8 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
     })->create();
+
+// Alihkan storage path ke /tmp agar bisa ditulis di Vercel
+$app->useStoragePath('/tmp/storage');
+
+return $app;
